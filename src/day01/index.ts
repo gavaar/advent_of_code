19dafcb85b5f95caf648d124f39c59ed50a40748
Gavaar <oxspit@gmail.com>
2021-12-02 17:43:11 +0100

    aoc day 1

---
+++ src/day01/index.ts
@@ -0,0 +1,31 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  console.log(input);
+  return;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      { input: `200`, expected: "" },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // { input: ``, expected: "" },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+});
